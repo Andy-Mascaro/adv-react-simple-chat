267d0a1005144e4cf962779924a729d8003cf90e
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-23 12:53:29 -0700

    fixed secrets on netlify and github

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+
 describe('example test', () => {
   it('first test', async () => {
     render(<MemoryRouter>
