dac0ab9a1b80e93fada3b4f1acd0bce692d06920
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-23 12:35:38 -0700

    added secrects to github and netlify

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-
 describe('example test', () => {
   it('first test', async () => {
     render(<MemoryRouter>
